51f27a9611354a0ea655f28af2f0678a6f071115
rex.idehen <rex.idehen2013@gmail.com>
2024-08-29 19:41:07 +0100

    assignment submit

---
+++ 3mttpython.py
@@ -0,0 +1,8 @@
+def convert_int_to_float(integer):
+
+    return float(integer)
+
+integer_input = int(input("Enter an integer: "))
+float_value = convert_int_to_float(integer_input)
+
+print("The converted float value is:", float_value)
